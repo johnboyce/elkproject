9ef2a97bc85775445231e6a1c72b3912db9bc0d8
johnboyce <johnboyce@comcast.net>
2025-01-11 16:37:12 -0500

    Add LBs to Service for update-service task Update healthcheck endpoint

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -21,7 +21,7 @@ resource "aws_lb_target_group" "main" {
   target_type = "ip"
 
   health_check {
-    path                = "/"
+    path                = "/healthcheck"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2

8b15ae28f097d675d48e5ebbe65a72536504595a
johnboyce <johnboyce@comcast.net>
2025-01-11 00:02:39 -0500

    Terraform - Add iam module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "elkproject-terraform-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket  = "elkproject-terraform-state"
+    key     = "global/s3/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
 
@@ -13,7 +13,7 @@ provider "aws" {
 
 # VPC Module
 module "vpc" {
-  source = "./modules/vpc"
+  source           = "./modules/vpc"
   application_name = var.application_name
 }
 
@@ -28,5 +28,10 @@ module "subnets" {
 module "security_groups" {
   source           = "./modules/security_groups"
   vpc_id           = module.vpc.vpc_id
+  application_name = var.application_name
+}
+
+module "iam" {
+  source           = "./modules/iam"
   application_name = var.application_name
 }

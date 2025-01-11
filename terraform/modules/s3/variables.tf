82086f1cdc8ce21f8c65be8b402483fbd0e8b383
johnboyce <johnboyce@comcast.net>
2025-01-10 23:23:43 -0500

    Action - convert to manual trigger

--- terraform/modules/s3/variables.tf
+++ terraform/modules/s3/variables.tf
@@ -2,3 +2,9 @@ variable "application_name" {
   description = "Name of the application"
   type        = string
 }
+
+variable "skip_bucket_creation" {
+  description = "Skip bucket creation if it already exists"
+  type        = bool
+  default     = true
+}

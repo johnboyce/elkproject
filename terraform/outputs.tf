ed43a9e0096f42569786470cab50556f41cbade3
johnboyce <johnboyce@comcast.net>
2025-01-10 23:40:11 -0500

    Terraform - Remove s3 output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,6 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "s3_bucket_name" {
-  value = module.s3.bucket_name
-}
-
 output "public_subnets" {
   value = module.subnets.public_subnets
 }
